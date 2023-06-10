bc578be93c086147b76d15c54a257b1ad4588475
Dohyun Moon <d6moon@uwaterloo.ca>
2023-06-09 23:51:55 -0400

    verification code and sudoku grid in pygame window

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
 import pygame
+import verification
 
-width = 800
+width = 540
 height = 600
-window = pygame.display.set_mode((width, height))
+grid_size = 60
+board_size = grid_size * 9
 
-pygame.display.set_caption("My Game")
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (200, 200, 200)
+BLUE = (0, 0, 255)
+
+window = pygame.display.set_mode((width, height))
+window.fill(WHITE)
+
+# initial empty grid
+grid = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+user_grid = [[0] * 9 for _ in range(9)]
+
+def draw_grid():
+    for i in range(10):
+        if i % 3 == 0:
+            thickness = 4
+        else:
+            thickness = 1
+        pygame.draw.line(window, BLACK, (0, i * grid_size), (board_size, i * grid_size), thickness)
+        pygame.draw.line(window, BLACK, (i * grid_size, 0), (i * grid_size, board_size), thickness)
+
+
+pygame.display.set_caption("Sudoku Solver")
 
 running = True
 while running:
@@ -13,8 +48,11 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    draw_grid()
     # Update game logic
 
     # Render the game
+
+    pygame.display.update()
 
 pygame.quit()
---
+++ verification.py
@@ -0,0 +1,47 @@
+def verify(self, board: list[list[str]]) -> bool:
+    # checking each row
+    for row in board:
+        data = {}
+        for cell in row:
+            if cell != '.':
+                if data.get(cell) == None:
+                    data.update({cell:1})
+                else:
+                    return False
+    
+    #checking each column
+    col = 0
+    while col < 9:
+        data = {}
+
+        for row in board:
+            if row[col] != '.':
+                if data.get(row[col]) == None:
+                    data.update({row[col]: 1})
+                else:
+                    return False
+
+        col += 1
+    
+    # checking each 3x3 blocks
+    row_mul = 3
+    for a in range(3):
+        for i in range(3):
+            data = {}
+            for j in range(3):
+                if board[i*row_mul][j + a*row_mul] != '.':
+                    if data.get(board[i*row_mul][j + a*row_mul]) == None:
+                        data.update({board[i*row_mul][j + a*row_mul]: 1})
+                    else:
+                        return False
+                if board[i*row_mul + 1][j + a*row_mul] != '.':
+                    if data.get(board[i*row_mul + 1][j + a*row_mul]) == None:
+                        data.update({board[i*row_mul + 1][j + a*row_mul]: 1})
+                    else:
+                        return False
+                if board[i*row_mul + 2][j + a*row_mul] != '.':
+                    if data.get(board[i*row_mul + 2][j + a*row_mul]) == None:
+                        data.update({board[i*row_mul + 2][j + a*row_mul]: 1})
+                    else:
+                        return False
+    return True
