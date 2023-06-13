71dce536a017fb662e943958bea4660daf28de04
Dohyun Moon <dohyundoor@gmail.com>
2023-06-13 17:07:02 -0400

    solve sudoku attempt

--- solve.py
+++ solve.py
@@ -1,12 +1,26 @@
-def solve(board: list[list[int]]) -> list[list[int]]: 
+def solve(board: list[list[int]]): 
+    def valid_input(row, col, val):
+        for i in range(9):
+            if board[row][i] != '.' and int(board[row][i]) == val: # check current row
+                return False
+            if board[i][col] != '.' and int(board[i][col]) == val: # check current column
+                return False
 
+            if board[3 * (row // 3) + i // 3][3 * (col // 3) + i % 3] != '.' and board[3 * (row // 3) + i // 3][3 * (col // 3) + i % 3] == val: # check current 3x3 box
+                return False
+        return True
 
-
-
-
-
-
-
-
-    
+    def actual_solve(init_row, init_col):
+        for row in range(init_row, 9):
+            for col in range(init_col, 9):
+                if board[row][col] == 0:
+                    for val in range(1, 10):
+                        if valid_input(row, col, val):
+                            board[row][col] = val
+                            actual_solve(row, col)
+                    
+                    return False
+        return True
+    actual_solve(0, 0)
+   
     return board[0], board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8] 
